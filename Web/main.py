b648f88e0d60a2acac4c31831165a8dcebc3a18f
Shyamal Ruparel <shyamalruparel1991@gmail.com>
2014-04-18 13:44:23 -0400

    Search working with tweepy

--- Web/main.py
+++ Web/main.py
@@ -1,10 +1,21 @@
-import twitter
+import tweepy
+import sys
 
 if __name__ == "__main__":
     config = {}
     execfile("config.conf", config)
-    api = twitter.Api(consumer_key=config["consumer_key"],
-                      consumer_secret=config["consumer_secret"],
-                      access_token_key=config["access_token_key"],
-                      access_token_secret=config["access_token_secret"])
-    print api.VerifyCredentials()
+
+    auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
+    auth.set_access_token(config["access_token_key"], config["access_token_secret"])
+
+    api = tweepy.API(auth)
+
+
+    tweets = api.search(q='#ucgrad14')
+    for tweet in tweets:
+        try:
+            if not ('RT @' in tweet.text):
+                print "@" + tweet.user.screen_name +": " +tweet.text
+        except:
+            # print sys.exc_info()[0]
+            i = 0
